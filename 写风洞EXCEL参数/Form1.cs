5bd123518b2f837806002dd8e55a3c99f6eb7de2
liu <liuxinyan0428@gmail.com>
2017-01-17 16:47:43 +0800

    将每个格子的参数写入写作一个方法的版本

--- 写风洞EXCEL参数/Form1.cs
+++ 写风洞EXCEL参数/Form1.cs
@@ -39,8 +39,30 @@ namespace WindowsFormsApplication1
             this.x2_RecZoom = decimal.Parse(x2_RectZoom.Text);
             this.y2_RecZoom = decimal.Parse(y2_RectZoom.Text);
             this.divideHori_RecZoom = Convert.ToInt32(divideHori_RectZoom.Text);
-            this.precision_RecZoom = decimal.Parse(precision_RectZoom.Text);
-            ///////////////////decimal[,] excelArea =new decimal[]
+           // this.precision_RecZoom = decimal.Parse(precision_RectZoom.Text);
+            string[,] excelArea = new string [divideZ_RecZoom+1,divideHori_RecZoom+1];
+            for (int i1 = 0; i1 < divideZ_RecZoom; i1++)
+            {
+                decimal z1 = (higherZ_RecZoom - lowerZ_RecZoom) / divideZ_RecZoom * i1 + lowerZ_RecZoom;
+                decimal z2=(higherZ_RecZoom - lowerZ_RecZoom) / divideZ_RecZoom * (i1+1) + lowerZ_RecZoom;
+                for (int i2 = 0; i2 < divideHori_RecZoom; i2++)
+                {
+                    decimal x1 = (x1_RecZoom - x2_RecZoom) / divideHori_RecZoom * i2 + x2_RecZoom;
+                    decimal x2 = (x1_RecZoom - x2_RecZoom) / divideHori_RecZoom * (i2+1) + x2_RecZoom;
+                    decimal y1 = (y1_RecZoom - y2_RecZoom) / divideHori_RecZoom * i2 + y2_RecZoom;
+                    decimal y2 = (y1_RecZoom - y2_RecZoom) / divideHori_RecZoom * (i2+1) + y2_RecZoom;
+                    if (precision_RectZoom.Text == null)
+                    {
+                        excelArea[i1, i2] = "Z(" + z1 + "," + z2 + ")A(" + x1 + "," + y1 + ")B(" + x2 + "," + y2 + ")";
+                    }
+                    else if (decimal.Parse(precision_RectZoom.Text) != 0)
+                    {
+                        excelArea[i1, i2] = "Z(" + z1 + "," + z2 + ")A(" + x1 + "," + y1 + ")B(" + x2 + "," + y2 + ")" + decimal.Parse(precision_RectZoom.Text);
+                    }                
+                }
+
+
+            }
         }
     }
 }
---
+++ 写风洞EXCEL参数/ParameterGenerate.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    class ParameterGenerate
+    {
+        public  ParameterGenerate()
+        {
+
+        }
+
+        public void OneGenerateRecArea(decimal[] para)
+        {
+            if (para.Length==6)       //应该是动态数组改为count
+            {
+                decimal z1 = para[0];
+                decimal z2 = para[1];
+                decimal x1 = para[2];
+                decimal y1 = para[3];
+                decimal x2 = para[4];
+                decimal y2 = para[5];
+                string cellPara = "Z(" + z1 + "," + z2 + ")A(" + x1 + "," + y1 + ")B(" + x2 + "," + y2 + ")";
+                //在click中写入excel表格
+            }
+
+            if (para.Length == 7)      //水平或者垂直区域，动态数组改为count
+            {
+                decimal z1 = para[0];
+                decimal z2 = para[1];
+                decimal x1 = para[2];
+                decimal y1 = para[3];
+                decimal x2 = para[4];
+                decimal y2 = para[5];
+                decimal precision =para[6];
+                string cellPara = "Z(" + z1 + "," + z2 + ")A(" + x1 + "," + y1 + ")B(" + x2 + "," + y2 + ")" + precision;
+                
+            }
+        }
+
+
+        //public void oneGenerateCuboid(decimal[] para)
+        //{
+        //    if (para.Length == 7)
+        //    {
+        //        decimal x0 = para[0];
+        //        decimal y0 = para[0];
+        //        decimal z0 = para[0];
+        //        decimal x1 = para[0];
+        //        decimal y1 = para[0];
+        //        decimal x2 = para[0];
+        //        decimal z1 = para[0];
+        //        //string cellPara = "Z(" + z1 + "," + z2 + ")A(" + x1 + "," + y1 + ")B(" + x2 + "," + y2 + ")" + precision;
+
+        //    }
+        //}
+    }
+}
